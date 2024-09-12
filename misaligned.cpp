1b655e5105abc0f3e274e0b27e24629582e0bc0e
Koushik Choudhury <koushik.choudhury@philips.com>
2024-09-12 21:10:09 +0530

    modified misaligned and added test

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,20 +1,45 @@
 #include <assert.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+void printColorPair(const char* majorColorArr[], const char* minorColorArr[], int i, int j, std::ostream& out)
+{
+    out << i * 5 + j << " | " << majorColorArr[i] << " | " << minorColorArr[i] << "\n";
+}
+
+void testPrintColorPair(const char* majorColorArr[], const char* minorColorArr[], int i, int j)
+{
+    std::stringstream ss;
+    printColorPair(majorColorArr, minorColorArr, i, j, ss);
+    //tests
+    std::string str;
+    std::getline(ss, str, '|');
+    assert((i * 5 + j) == std::stoi(str));
+    std::getline(ss, str, '|');
+    str.erase(std::remove_if(str.begin(), str.end(), std::isspace), std::end(str));
+    assert(majorColorArr[i] == str);
+    std::getline(ss, str, '|');
+    str.erase(std::remove_if(str.begin(), str.end(), std::isspace), std::end(str));
+    assert(minorColorArr[j] == str);
+}
+
+int printColorMap(const char** majorColorArr, const char** minorColorArr) {
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            printColorPair(majorColorArr, minorColorArr, i, j, std::cout);
         }
     }
     return i * j;
 }
 
 int main() {
-    int result = printColorMap();
+    const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    int result = printColorMap(majorColor, minorColor);
+    testPrintColorPair(majorColor, minorColor, 1, 2);
     assert(result == 25);
     std::cout << "All is well (maybe!)\n";
     return 0;
