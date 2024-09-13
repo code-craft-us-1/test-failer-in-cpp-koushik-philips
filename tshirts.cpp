116a0c1965a983bd3f373bd1883320deb411cee9
Koushik Choudhury <koushik.choudhury@philips.com>
2024-09-13 11:47:02 +0530

    refactored tshirts

--- tshirts.cpp
+++ tshirts.cpp
@@ -14,10 +14,10 @@ char size(int cms) {
 }
 
 int main() {
-    assert((size(38) == 'M') && (size(43) == 'L'));
+    assert((size(38) == 'M') && (size(42) == 'L'));
     assert(size(37) == 'S');
     assert(size(40) == 'M');
-    assert(size(42) == 'L');
+    assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
