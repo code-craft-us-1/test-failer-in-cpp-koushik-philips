46f8fad1da4f51986578e3e23278b1a8b21a7621
Koushik Choudhury <koushik.choudhury@philips.com>
2024-09-13 11:40:01 +0530

    refactor misaligned

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,52 +3,52 @@
 #include <sstream>
 #include <string>
 #include <algorithm>
-#include <regex>
 
-int printColorMap(std::ostream& out) {
-    const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+void printColorPair(const char* majorColorArr[], const char* minorColorArr[],
+    int i, int j, std::ostream& out) {
+    out << i * 5 + j << " | " << majorColorArr[i] << " | " << minorColorArr[i] << "\n";
+}
+
+int printColorMap() {
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            out << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            printColorPair(majorColor, minorColor, i, j, std::cout); // core part
         }
     }
     return i * j;
 }
 
-int testPrintColorMap()
-{
-    std::stringstream ss;
-    int result = printColorMap(ss);
-
-    int nNumEntriesInPrint = 0;
+// Test - core part
+int testPrintColorMap() {
     const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
     const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-    std::string inputStr;
-    int nMajorIdx = 0, nMinorIdx = 0;
-    while (std::getline(ss, inputStr, '\n'))
-    {
-        std::regex r("(^[0-9]+ )[|] ([a-zA-Z]+) [|] ([a-zA-Z]+)$");
-        std::smatch s;
-        assert(std::regex_search(inputStr, s, r));                  // Alignment of "|" check
-        {
-            assert((nMajorIdx * 5 + nMinorIdx) == std::stoi(s[1])); // Number check
-            assert(majorColor[nMajorIdx] == s[2]);                  // MajorColor check
-            assert(minorColor[nMinorIdx] == s[3]);                  // MinorColor check
-            nMinorIdx++;
-            if (nMinorIdx == 5) {
-                nMinorIdx = 0;
-                nMajorIdx++;
+    int retNumSucessTests = 0;
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++) {
+            std::stringstream ss;
+            printColorPair(majorColor, minorColor, i, j, ss);
+            // Donot stop test at first failure.
+            // Count sucessfull & unsucessfull tests
+            auto str = std::to_string((i * 5 + j)) + " | " + std::string(majorColor[i]) +
+                " | " + std::string(minorColor[j]);
+            std::string inputStr;
+            std::getline(ss, inputStr);
+            if (str == inputStr) {
+                std::cout << "Test Successfull for: " << "(" << i << "," << j << ")" << "\n";
+                retNumSucessTests++;
+            }
+            else {
+                std::cout << "Test FAILED for: " << "(" << i << "," << j << ")" << "\n";
             }
         }
-        nNumEntriesInPrint++;
-    }
-    return nNumEntriesInPrint;
+    return retNumSucessTests;
 }
 
 int main() {
-    int result = printColorMap(std::cout);
+    int result = printColorMap();
     assert(result == 25);
     assert(testPrintColorMap() == 25);
     std::cout << "All is well (maybe!)\n";
