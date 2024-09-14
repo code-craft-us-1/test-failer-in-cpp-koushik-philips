3251480b72e97d28b4cc4894a2eb2f281fcd5133
Koushik Choudhury <koushik.choudhury@philips.com>
2024-09-14 10:04:01 +0530

    refactor misaligned

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,53 +3,69 @@
 #include <sstream>
 #include <string>
 #include <algorithm>
+#include <tuple>
 
-void printColorPair(const char* majorColorArr[], const char* minorColorArr[],
-    int i, int j, std::ostream& out) {
-    out << i * 5 + j << " | " << majorColorArr[i] << " | " << minorColorArr[i] << "\n";
-}
-
-int printColorMap() {
+int printColorMap(std::ostream& out) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            printColorPair(majorColor, minorColor, i, j, std::cout);  // core part
+            out << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
 }
 
 // Test - core part
-int testPrintColorMap() {
+std::tuple<int,int,int,int> testPrintColorMap() {
+    std::stringstream ss;
+    int result = printColorMap(ss);
+    // test
     const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
     const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-    int retNumSucessTests = 0;
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < 5; j++) {
-            std::stringstream ss;
-            printColorPair(majorColor, minorColor, i, j, ss);
-            // Donot stop test at first failure.
-            // Count sucessfull tests and return it
-            auto str = std::to_string((i * 5 + j)) + " | " + std::string(majorColor[i]) +
-                " | " + std::string(minorColor[j]);
-            std::string inputStr;
-            std::getline(ss, inputStr);
-            if (str == inputStr) {
-                std::cout << "Test Successfull for: " << "(" << i << "," << j << ")" << "\n";
-                retNumSucessTests++;
-            } else {
-                std::cout << "Test FAILED for: " << "(" << i << "," << j << ")" << "\n";
-            }
+    int nCntNumMatch = 0, nCntMajorMatch = 0, nCntMinorMatch = 0, nCntFullMatch = 0;
+    std::string inputStr;
+    int i = 0, j = 0;
+    while (std::getline(ss, inputStr, '\n')) {
+        std::string numStr,  majorStr, minorStr, partitionStr;
+        std::stringstream inputSS(inputStr);
+        inputSS >> numStr >> partitionStr >> majorStr >> partitionStr >> minorStr;
+        auto expectedNumStr = std::to_string(i * 5 + j);
+        auto expectedMajorStr = majorColor[i];
+        auto expectedMinorStr = minorColor[j];
+        if (expectedNumStr == numStr) {
+            nCntNumMatch++;
         }
-    return retNumSucessTests;
+        if (expectedMajorStr == majorStr) {
+            nCntMajorMatch++;
+        }
+        if (expectedMinorStr == minorStr) {
+            nCntMinorMatch++;
+        }
+        std::string expectedStr = expectedNumStr + " | " + expectedMajorStr
+                                  + " | " + expectedMinorStr;
+        if (expectedStr == inputStr) {
+            nCntFullMatch++;
+        }
+        j++;
+        if (j == 5)
+        {
+            j = 0;
+            i++;
+        }
+    }
+    return std::make_tuple(nCntNumMatch, nCntMajorMatch, nCntMinorMatch, nCntFullMatch);
 }
 
 int main() {
-    int result = printColorMap();
+    int result = printColorMap(std::cout);
     assert(result == 25);
-    assert(testPrintColorMap() == 25);
+    auto [num, major, minor, full] = testPrintColorMap();
+    assert(num == 25);      // Number match
+    assert(major == 25);    // Major-colors match
+    assert(minor == 25);    // Minor-colors match
+    assert(full == 25);     // Full match
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
